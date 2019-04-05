14aeb49577b586324802703eb280376450e80e79
darshanpatidar1 <darshancoding@gmail.com>
2019-04-05 18:46:56 +0530

    c

--- airtravel.py
+++ airtravel.py
@@ -1,8 +1,11 @@
 """Model for aircraft flights"""
 
-class Flight:
+# from pprint import pprint as pp
 
-    def __init__(self,number):
+class Flight:
+    """A Flight with a particular passenger aircraft"""
+
+    def __init__(self,number,aircraft):
         if not number[:2].isalpha():
             raise ValueError("No airline code in '{}'".format(number))
 
@@ -13,12 +16,46 @@ class Flight:
             raise ValueError("Invalid route number '{}'".format(number))
 
         self._number = number
+        self._aircraft = aircraft
+
+        rows,seats=self._aircraft.seating_plan()
+        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
 
     def number(self):
         return self._number
 
     def airline(self):
         return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+    def aircraft_registration(self):
+        return self._aircraft.registration()
+
+    def allocate_seat(self,seat,passenger):
+        """Allocate a seat to passenger"""
+
+        rows,seat_letters=self._aircraft.seating_plan()
+
+        letter = seat[-1]
+
+        if letter not in seat_letters:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text=seat[:-1]
+        try:
+            row=int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+
+        self._seating[row][letter] = passenger
 
 class Aircraft:
 
@@ -39,15 +76,19 @@ class Aircraft:
                 "ABCDEFGHJK"[:self._num_seats_per_row])
 
 if __name__== '__main__':
+#
+#     # f=Flight("SN309")
+#     # print("Flight Number - ",f.number())
+#     # print("Airline Number - ",f.airline())
+#
+#     a= Aircraft("G-EUPT","Airbus A319",22,num_seats_per_row=6)
+#     print(a.registration())
+#     print(a.model())
+#     print(a.seating_plan())
 
-    # f=Flight("SN309")
-    # print("Flight Number - ",f.number())
-    # print("Airline Number - ",f.airline())
-
-    a= Aircraft("G-EUPT","Airbus A319",22,num_seats_per_row=6)
-    print(a.registration())
-    print(a.model())
-    print(a.seating_plan())
+    f = Flight("SN309", Aircraft("G-EUPT", "Airbus A319", 22, num_seats_per_row=6))
+    print(f.aircraft_model())
+    print(f.aircraft_registration())
 
     # a=Aircraft('3349AA','AAA39',20,10)
     # print(a.registration())
