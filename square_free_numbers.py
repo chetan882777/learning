37d25bf38e6c3081b9c0f8d02b4ecd365b0d08b3
darshanpatidar1 <darshancoding@gmail.com>
2019-06-21 15:58:56 +0530

    c

--- square_free_numbers.py
+++ square_free_numbers.py
@@ -1,21 +1,16 @@
+from functools import reduce
 from math import sqrt
 def is_square(num):
     if sqrt(num)==int(sqrt(num)): return True
     else: return False
 
-def factors(num):
-    factors=[]
-    for val in range(2,num+1):
-        if num/val==num//val: factors.append(val)
-    return factors
+def factors(n):
+    step = 2 if n % 2 else 1
+    return list(sorted(set(reduce(list.__add__,([i, n // i] for i in range(1, int(sqrt(n)) + 1, step) if n % i == 0)))))
 
 def is_square_free(num):
     temp=[]
-    for val in factors(num):
-        temp.append(not(is_square(val)))
-    return all(temp)
+    [temp.append(not (is_square(val))) for val in factors(num)]
+    return all(temp[1:])
 
-while(1):
-    print((factors(int(input()))))
-    # found=[]
-    # print(len([val for val in factors(int(input())) if is_square_free(val)]))
+print(len(([val for val in factors(int(input())) if is_square_free(val)])[1:]))
